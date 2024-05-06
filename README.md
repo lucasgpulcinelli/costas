cd703728a94e2e12e64a6e41202a62789f9c84d7
lucasgpulcinelli <lucasegp@usp.br>
2024-05-06 14:50:36 -0300

    add costas code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # costas
-Costas (back in portuguese) is a backup script I use every week. 
+Costas (back in portuguese) is a backup script I use every week. It creates encrypted snapshots of directories with an ignore list to prevent bloat and slowdowns.
+
+## Setup
+First, create a gpg key pair. Then, you should copy the key ID and write the configuration file.
+
+## Configuration
+Costas needs to be configured properly in order to work. Create a file (or copy it from `examples/costas.json`) in `~/.config/costas/costas.json` and add the following data:
+* "gpg-keyid", with the gpg key ID for the recipent you want costas to encript to (probably your own key),
+* "base-directory", with the directory the tar command will execute,
+* "directories", with a list of directories relative to the "base-directory" costas will encrypt, and
+* "ignore", with a list of directories/files relative to the "base-directory" not to encrypt, taking precedence over "directories".
+* "output-directory", with the output file diretory to store. Note that if it is inside the "diretories" list, problems might occur.
+
+## Running costas
+Then, if you want this program to create a single snapshot, run `./costas.py`.
---
+++ costas.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from subprocess import Popen, PIPE
+import datetime
+import json
+import os
+
+OPTIONS_FILE = os.path.join(
+    os.environ["HOME"], ".config", "costas", "costas.json")
+
+
+def main(options):
+    keyid = options["gpg-keyid"]
+    base_dir = options["base-directory"]
+    directories = options["directories"]
+    ignore = options["ignore"]
+    output_directory = options["output-directory"]
+
+    assert type(keyid) == str
+    assert os.path.isdir(base_dir)
+    for d in directories:
+        assert os.path.isdir(os.path.join(base_dir, d))
+    for i in ignore:
+        assert os.path.exists(i)
+    assert os.path.isdir(output_directory)
+
+    filename = datetime.date.today().strftime("%y-%m-%d")
+
+    args = ["tar"]
+    for i in ignore:
+        args.append(f"--exclude='{base_dir}'")
+    args.extend(["czf", "-"])
+    args.extend(directories)
+
+    tarout = Popen(args, cwd=base_dir, stdout=PIPE)
+
+    gpgfile = open(f"{output_directory}/{filename}.gpg", "w")
+    Popen(["gpg", "-r", keyid, "--encrypt", "-"],
+          stdin=tarout.stdout, stdout=gpgfile).wait()
+    gpgfile.close()
+
+
+if __name__ == '__main__':
+    f = open(OPTIONS_FILE)
+    options = json.load(f)
+    f.close()
+
+    main(options)
